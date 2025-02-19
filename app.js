9bc5d75d80cd4dafc30ab3bfed48adf28b660d23
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2025-02-19 14:44:06 -0500

    New Features

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-
 import express, {json} from "express";
 import {webhooksRouter} from "./src/routes/webhooks.js";
+
 const app = express()
 
 app.use(json())
 app.disable('x-powered-by')
-//process.loadEnvFile()
-
 
 
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-    res.json({message:"Hello World"})
+    res.json({message: "Hello World"})
 })
 
 app.use('/webhooks', webhooksRouter);
@@ -23,10 +21,13 @@ app.locals.timeTokenTravelC = Date.now();
 app.locals.timeTokenZoho = Date.now();
 
 
-app.listen(PORT, () => {
-    //console.log(`server listening on port http://localhost:${PORT}`)
-    console.log('app.locals.timeTokenTravelC del APP.JS: ', app.locals.timeTokenTravelC)
-    //console.log('process.env.WEBHOOK_KEY APP.JS: ', `/travelc/bookings/${process.env.WEBHOOK_KEY}`)
-})
+// Solo iniciar el servidor en producción (no en el build)
+if (process.env.NODE_ENV !== "build") {
+    app.listen(PORT, () => {
+        //console.log(`server listening on port http://localhost:${PORT}`);
+        console.log("app.locals.timeTokenTravelC del APP.JS: ", app.locals.timeTokenTravelC);
+        //console.log("process.env.WEBHOOK_KEY APP.JS: ", `/travelc/bookings/${process.env.WEBHOOK_KEY}`);
+    });
+}
 
 export default app
