90ea47ac3a90df9e9c7c8b512881f2764fc394cb
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2025-02-19 16:31:10 -0500

    New Features

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -9,7 +9,7 @@ export class webhookController {
 
         //recibir los datos del request
         const {micrositeId, bookingReference, type} = req.body;
-        console.log('bookingReference CONTROLLER.JS: ',bookingReference);
+        console.log('bookingReference CONTROLLER.JS: ', bookingReference);
 
         //verificar los datos del request
         if (micrositeId !== 'zettatravel') {
@@ -20,34 +20,46 @@ export class webhookController {
         //console.log('El micrositio es zettatravel CONTROLLER.JS')
         res.status(200).json({message: 'Webhook recibido'});
 
-        console.log('Controller Date.now() CONTROLLER.JS: ',Date.now())
-        console.log('Controller app.locals.timeTokenTravelC CONTROLLER.JS: ',app.locals.timeTokenTravelC)
+        console.log('Controller Date.now() CONTROLLER.JS: ', Date.now())
+        console.log('Controller app.locals.timeTokenTravelC CONTROLLER.JS: ', app.locals.timeTokenTravelC)
 
         if (Date.now() >= app.locals.timeTokenTravelC) {
-            console.log('Date.now() es mayor o igual que el tiempo del token, debe autenticarse CONTROLLER.JS')
-            await authTravelC.auth(micrositeId).then(() => console.log('Autenticado CONTROLLER.JS'));;
-        }else{
-            console.log('Date.now() es menor que tiempo del token, esta dentro de la hora (no se autentica) CONTROLLER.JS')
+            console.log('Date.now() de TC es mayor o igual que el tiempo del token, debe autenticarse CONTROLLER.JS')
+
+            try {
+                await authTravelC.auth(micrositeId);
+                console.log('Autenticado en TravelC CONTROLLER.JS');
+            } catch (error) {
+                console.error("No Autenticando en TravelC CONTROLLER.JS", error);
+            }
+
+        } else {
+            console.log('Date.now() de TC es menor que tiempo del token, esta dentro de la hora (no se autentica) CONTROLLER.JS')
         }
 
         //realizar la busqueda de reserva
         const bookings = await booking.getBookings(bookingReference, micrositeId);
-        console.log('Realizo la busqueda CONTROLLER.JS: ',bookings.id )
+        console.log('Realizo la busqueda CONTROLLER.JS: ', bookings.id)
 
         //realizar proceso de zoho
 
         if (Date.now() >= app.locals.timeTokenZoho) {
-            console.log('Date.now() es mayor o igual que el tiempo del token, debe autenticarse CONTROLLER.JS')
-            await authZoho.auth().then(() => console.log('Autenticado ZOHO CONTROLLER.JS'));
-        }else{
-            console.log('Date.now() es menor que tiempo del token, esta dentro de la hora (no se autentica) CONTROLLER.JS')
+            console.log('Date.now() de ZOHO es mayor o igual que el tiempo del token, debe autenticarse CONTROLLER.JS')
+
+            try {
+                await authZoho.auth();
+                console.log('Autenticado en Zoho CONTROLLER.JS');
+            } catch (error) {
+                console.error("No Autenticando en Zoho CONTROLLER.JS", error);
+            }
+
+        } else {
+            console.log('Date.now() de ZOHO es menor que tiempo del token, esta dentro de la hora (no se autentica) CONTROLLER.JS')
         }
 
         //una vez autenticado se realizan dos cosas,
         //1. se crea el contacto en zoho
         //2. se crea la reserva en zoho
-
-
 
 
     }
