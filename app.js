7c32295c4cd6628c4483f62b953f9a7dda707cdd
Felipe Garcia <felipe-garcia@javeriana.edu.co>
2025-06-09 09:34:03 -0500

    Refactor app.js to improve middleware structure and remove commented code

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import {webhooksRouter} from "./src/routes/webhooks.js";
-import {logger} from "./src/utils/logUtils.js";
+import {corsMiddleware} from "./src/config/cors.js";
 
-const app = express()
+const app = express();
 
-app.use(json())
-
-
- // middelware de cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    next();
-});
-
-
-app.disable('x-powered-by')
-
-
-const PORT = process.env.PORT ?? 3000
-
-/*
-Prueba de que funciona
-app.get('/', (req, res) => {res.json({message: "Hello World"})})
-*/
+app.use(json());
+app.use(corsMiddleware);
+app.disable('x-powered-by');
 
 app.use('/webhooks', webhooksRouter);
 
+// Inicializar tokens en memoria
 app.locals.authTokenTravelC = null;
 app.locals.authTokenZoho = null;
 app.locals.timeTokenTravelC = Date.now();
 app.locals.timeTokenZoho = Date.now();
 
-
-app.listen(PORT, () => {
-    logger.debug(`Server running on http://localhost:${PORT}`);
-    logger.debug(`Enviroment: ${process.env.NODE_ENV}`);
-});
-
-
-export default app
+export default app;
